c4ba37ef85722dcdb12cd358db69a75ceb0ad59b
christopher snow <chsnow123@gmail.com>
2015-10-21 18:16:16 +0100

    Ireland demo 22 Oct 2015

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,22 @@
 angular.module('TodoNgPouchdb', ['ionic','pouchdb'])
   .controller('TodoCtrl', function($scope, $ionicPopup, $ionicListDelegate, pouchCollection) {
 
-    var dbName = 'todo-ng-pouchdb';
+    var dbName = 'ireland_demo';
+    var userid = 'chris.snow';
+    var url = 'https://ireelimemeneveredstainli:2744c65d0fff0689ad703385786044bf720ad612@rmgdemo2.cloudant.com/' + dbName;
+
+
+
     $scope.tasks = pouchCollection(dbName);
 
     $scope.toggleCompletion = function(task) {
       task.completed = !task.completed;
+      if (task.completed) {
+          var dt = new Date();
+          task.date_completed = dt.toISOString();
+      } else {
+          delete task.date_completed;
+      }
       $scope.tasks.$update(task);
     };
 
@@ -15,23 +26,31 @@ angular.module('TodoNgPouchdb', ['ionic','pouchdb'])
 
     $scope.newTask = function() {
       $ionicPopup.prompt({
-        title: "New Task",
+        task_name: "New Task",
         template: "Enter task:",
         inputPlaceholder: "What do you need to do?",
         okText: 'Create task'
       }).then(function(res) {    // promise
-        if (res) $scope.tasks.$add({title: res, completed: false});
+        var dt = new Date();
+        if (res) $scope.tasks.$add(
+            {
+                userid: userid,
+                task_name: res, 
+                date_created: dt.toISOString(), 
+                completed: false
+            }
+        );
       })
     };
 
     $scope.editTask = function(task) {
-      $scope.data = { response: task.title }; // A hack to pre-populate prompt
+      $scope.data = { response: task.task_name }; // A hack to pre-populate prompt
       $ionicPopup.prompt({
         title: "Edit Task",
         scope: $scope
       }).then(function(res) {    // promise
-        if (res !== undefined && task.title !== $scope.data.response) {
-          task.title = $scope.data.response;
+        if (res !== undefined && task.task_name !== $scope.data.response) {
+          task.task_name = $scope.data.response;
           $scope.tasks.$update(task);
         } // response not res has new title (hack)
         $ionicListDelegate.closeOptionButtons();
@@ -42,7 +61,7 @@ angular.module('TodoNgPouchdb', ['ionic','pouchdb'])
     $scope.toggleOnline = function() {
       $scope.online = !$scope.online;
       if ($scope.online) {  // Read http://pouchdb.com/api.html#sync
-        $scope.sync = $scope.tasks.$db.replicate.sync('http://127.0.0.1:5984/' + dbName, {live: true})
+        $scope.sync = $scope.tasks.$db.replicate.sync(url, {live: true})
           .on('error', function (err) {
             console.log("Syncing stopped");
             console.log(err);
